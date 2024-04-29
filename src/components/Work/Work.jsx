139bf0f1d71fd3d11fc1a8c40700d60a08adcf86
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-04-30 02:27:05 +0530

    initial

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Work.css'
+
+function Work() {
+    return (
+        <div className="container mt-5">
+            <div className='timeline ml-67'>
+                <div className="work-exp-box left">
+                    <div className="content">
+                        <h2>Wordpress Fullstack Developer</h2>
+                        <p className='company-name'>Belnet E Marketing OPC</p>
+                        <span className='company-year'>2019 - 2021</span>
+                        <p>Expert in WordPress development, mastering both front and back-end.</p>
+                    </div>
+                </div>
+                <div className="work-exp-box right">
+                    <div className="content">
+                        <h2>Wordpress Fullstack Developer</h2>
+                        <p className='company-name'>Belnet E Marketing OPC</p>
+                        <span className='company-year'>2019 - 2021</span>
+                        <p>Expert in WordPress development, mastering both front and back-end.</p>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Work
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import StickyFollow from '../components/StickyFollow/StickyFollow'
 import Heading from '../components/Heading/Heading'
+import Work from '../components/Work/Work'
 
 function Experience() {
   return (
     <div className='container-fluid'>
         <Heading data={{title:'Experience'}} />
+        <Work />
         <StickyFollow />
     </div>
   )
