a6f2885d6ac44ee612b74903ec5833f09ab1540d
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-07-05 02:44:48 +0530

    Node Mailer & Media Query

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import nodemailer from 'nodemailer';
 import './Form.css'
 
 function Form() {
+
+    const [Fname, setFname] = useState("");
+    const [Lname, setLname] = useState("");
+    const [email, setEmail] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 587,
+        secure: false, // Use `true` for port 465, `false` for all other ports
+        auth: {
+          user: "dev@tapabrata.me",
+          pass: "tapa8637@A",
+        },
+    });
+
+    const sendMail = async ()=>{
+        const info = await transporter.sendMail({
+            from: '"Tapabrata Goswami 👻" <dev@tapabrata.me>', // sender address
+            to: email, // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: msg, // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+        return info;
+    }
+
+    const submit = (e) =>{
+        e.preventDefault();
+        console.log(sendMail());
+    }
+
   return (
     <div className='container my-3'>
         <div className='row justify-content-center ml-100 get-in-touch-contaier'>
@@ -22,6 +55,8 @@ function Form() {
                                 type="text" 
                                 placeholder='First Name'
                                 className='form-control'
+                                // value={Fname}
+                                onChange={(e)=>setFname(e.target.value)}
                                 />
                         </div>
                         
@@ -32,6 +67,7 @@ function Form() {
                                     type="text" 
                                     placeholder='Last Name'
                                     className='form-control'
+                                    onChange={(e)=>setLname(e.target.value)}
                                     />
                             </div>
                     </div>
@@ -41,13 +77,14 @@ function Form() {
                     type="email" 
                     className='form-control'
                     placeholder='Email' 
+                    onChange={(e)=>setEmail(e.target.value)}
                     />
                 </div>
                 <div className='form-group mt-2'>
-                <textarea className="form-control" rows="5"></textarea>
+                <textarea className="form-control" rows="5" onChange={(e)=>setMsg(e.target.value)}></textarea>
                 </div>
                 <div className='from-group'>
-                    <button className='btn btn-primary mt-2 download-resume'>Submit</button>
+                    <button className='btn btn-primary mt-2 download-resume' onClick={(e) => submit(e)}>Submit</button>
                 </div>
             </form> 
             </div>
